795a04c11361c737c7a7158551356f95445213e5
rudolfix <rudolfix@rudolfix.org>
2026-06-09 17:06:34 +0200

    test and docs fixes

--- tests/libs/test_ibis.py
+++ tests/libs/test_ibis.py
@@ -62,6 +62,7 @@ def test_list_tables(populated_pipeline: dlt.Pipeline):
         "items",
         "double_items",
         "orderable_in_chain",
+        "daily_items",
         "_dlt_pipeline_state",
         "items__children",
     ]
